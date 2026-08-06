5bc3511375b664621bbb5dbb93bf5ac0badbfac0
mubashirtaj <mubashirtajuddin161280@gmail.com>
2026-08-06 15:29:01 +0500

    Fall back to module build info for --version under go install
    go install .../ghostport@version doesn't run GoReleaser's ldflags, so
    the binary always reported "dev". Read the version Go embeds in build
    info when the ldflag-set version is still the zero value.

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/mubashirtaj/ghostport/cmd"
+import (
+	"runtime/debug"
+
+	"github.com/mubashirtaj/ghostport/cmd"
+)
 
 // version is set at build time via:
 //
@@ -8,6 +12,19 @@ import "github.com/mubashirtaj/ghostport/cmd"
 var version = "dev"
 
 func main() {
-	cmd.Version = version
+	cmd.Version = resolveVersion()
 	cmd.Execute()
+}
+
+// resolveVersion falls back to the module version Go embeds automatically
+// when installed via `go install .../ghostport@version`, since that path
+// doesn't run GoReleaser's ldflags.
+func resolveVersion() string {
+	if version != "dev" {
+		return version
+	}
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+	return version
 }
